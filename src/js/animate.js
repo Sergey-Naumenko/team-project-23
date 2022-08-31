f378faef21d829ef0b5cd2a169f9f6707c5cadd2
Sergey-naumenko <sergnaumenko1@gmail.com>
2022-09-01 01:28:16 +0300

    add animation

---
+++ src/js/animate.js
@@ -0,0 +1,6 @@
+// All animations will take twice the time to accomplish
+document.documentElement.style.setProperty('--animate-duration', '1s');
+
+// All animations will take half the time to accomplish
+document.documentElement.style.setProperty('--animate-duration', '1s');
+
